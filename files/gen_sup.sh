40ee983808409205cf0684883c23f83995361b24
Maxwell Rathbone <mrathbone@orbitalgrooves.com>
2021-11-13 11:41:24 -0500

    switch back to bash

---
+++ files/gen_sup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "[program:$1]";
+echo "process_name  = $1";
+echo "autostart     = true";
+echo "autorestart   = false";
+echo "directory     = /";
+echo "command       = $2";
+echo "startsecs     = 3";
+echo "priority      = 1";
