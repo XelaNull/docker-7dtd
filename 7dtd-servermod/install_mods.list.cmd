734092bcf419c5e17f081f51c4e1711004ba7fb8
Dr. <xelanull@orbitalgrooves.com>
2024-08-12 18:54:08 -0400

    updated mod listing

--- 7dtd-servermod/install_mods.list.cmd
+++ 7dtd-servermod/install_mods.list.cmd
@@ -16,5 +16,17 @@ wget_download "https://illy.bz/fi/7dtd/server_fixes.tar.gz" server_fixes.tgz ext
 
 wget_download "https://bitbucket.org/obsessive-coder/sevendaystodie-servertools/downloads/7dtd-ServerTools-G1.0.1.6.zip" ServerTools.zip extract_file # ServerTools
 
-wget_download "https://github.com/XelaNull/7DTD-Neopolitan/releases/download/A19.3_1.0d/2021-06-03_215241-Modlet_Collection-Shouden.zip" Neopolitan.zip extract_file
+#wget_download "https://github.com/XelaNull/7DTD-Neopolitan/releases/download/A19.3_1.0d/2021-06-03_215241-Modlet_Collection-Shouden.zip" Neopolitan.zip extract_file
 
+git_clone https://github.com/XelaNull/7DTD-WhiteRiver_ToC.git
+git_clone https://github.com/XelaNull/7dtd-Origin-UI.git
+git_clone https://github.com/XelaNull/7DTD-ShoudensModlets.git
+git_clone https://github.com/XelaNull/7dtd-FixedModlets.git
+git_clone https://github.com/XelaNull/7DTD-Bag_Bellows_Headshot.git
+git_clone https://github.com/XelaNull/7DTD-Zombiepedia_Skillpoints.git
+git_clone https://github.com/XelaNull/7DTD-ZombieLootbag_Increase.git
+git_clone https://github.com/XelaNull/7DTD-Magazine_Plants_Trader.git
+git_clone https://github.com/XelaNull/7DTD-DarkerNights_BrighterLights.git
+git_clone https://github.com/XelaNull/7DTD-SalvagedElectronics.git
+git_clone https://github.com/XelaNull/7DTD-SanitysEdgeRules.git
+git_clone https://github.com/XelaNull/7dtd-Origin-UI.git
