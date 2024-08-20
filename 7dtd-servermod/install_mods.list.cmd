2c342f7b623dc7a05e462b3a13c7ead174cf3f36
Dr. <xelanull@orbitalgrooves.com>
2024-08-19 21:44:21 -0400

    updates

--- 7dtd-servermod/install_mods.list.cmd
+++ 7dtd-servermod/install_mods.list.cmd
@@ -4,12 +4,6 @@
 # Last Updated: 2024-08-11
 # For 1.0
 
-#((MODCOUNT++))
-#mkdir $MODS_DIR/$MODCOUNT
-#mv /data/7DTD/7dtd-servermod/best_mod.zip $MODS_DIR/$MODCOUNT
-#cd $MODS_DIR/$MODCOUNT
-#unzip best_mod.zip
-
 wget_download "https://illy.bz/fi/7dtd/server_fixes.tar.gz" server_fixes.tgz extract_file
 git_clone https://github.com/Prisma501/Allocs-Webmap-for-CPM.git
 mv $MODCOUNT/Allocs-Webmap-for-CPM/map.js $MODS_DIR/1/Mods/Allocs_WebAndMapRendering/webserver_legacy/js
@@ -25,6 +19,7 @@ git_clone https://github.com/XelaNull/7dtd-Origin-UI.git
 git_clone https://github.com/XelaNull/7DTD-ShoudensModlets.git
 git_clone https://github.com/XelaNull/7dtd-FixedModlets.git
 git_clone https://github.com/XelaNull/7DTD-Bag_Bellows_Headshot.git
+rm -rf $MODCOUNT/7DTD-Bag_Bellows_Headshot/Headshot_25*
 git_clone https://github.com/XelaNull/7DTD-Zombiepedia_Skillpoints.git
 git_clone https://github.com/XelaNull/7DTD-ZombieLootbag_Increase.git
 git_clone https://github.com/XelaNull/7DTD-Magazine_Plants_Trader.git
@@ -32,6 +27,7 @@ git_clone https://github.com/XelaNull/7DTD-DarkerNights_BrighterLights.git
 git_clone https://github.com/XelaNull/7DTD-SalvagedElectronics.git
 git_clone https://github.com/XelaNull/7DTD-SanitysEdgeRules.git
 git_clone https://github.com/XelaNull/7DTD-Skillbook_Skillpoints.git
+rm -rf $MODCOUNT/7DTD-Skillbook_Skillpoints/All* $MODCOUNT/7DTD-Skillbook_Skillpoints/Most*
 
 git_clone https://github.com/DonovanMods/donovan-7d2d-modlets.git
 mv $MODCOUNT/donovan-7d2d-modlets/modlets/donovan-more* $MODCOUNT/donovan-7d2d-modlets/modlets/donovan-night* $MODCOUNT/donovan-7d2d-modlets/modlets/a-la-carte/donovan-betterbridges $MODCOUNT/donovan-7d2d-modlets/modlets/a-la-carte/donovan-betterbuffs $MODCOUNT/donovan-7d2d-modlets/modlets/a-la-carte/donovan-betterdyes $MODCOUNT/donovan-7d2d-modlets/modlets/a-la-carte/donovan-bettertraps $MODCOUNT/donovan-7d2d-modlets/modlets/a-la-carte/donovan-pickmeup $MODCOUNT/
