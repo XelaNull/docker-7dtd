e804b0d3a01157394df7a349cc101aac8f38cfcd
Dr. <xelanull@orbitalgrooves.com>
2024-08-19 22:13:18 -0400

    updates

--- 7dtd-servermod/default_mods.sh
+++ 7dtd-servermod/default_mods.sh
@@ -6,9 +6,14 @@ ln -s "`find $INSTALL_DIR/Mods-Available -name \"ModInfo.xml\" | sed 's|/ModInfo
 
 cd $INSTALL_DIR/Mods
 
-#remap Allocs_CommandExtensions
-#remap Allocs_CommonFunc
-#remap Allocs_WebAndMapRendering
-#remap Botman
+remap Allocs_CommandExtensions
+remap Allocs_CommonFunc
+remap Allocs_WebAndMapRendering
 remap 1CSMM_Patrons
 remap ServerTools
+remap Origin_UI
+remap 60Slot_Bag_30Reserve
+remap 7DTD-8CraftQueue
+remap 7DTD-FasterCrafting
+remap Faster_Bellows
+remap Pickup_Plants
